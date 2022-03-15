89b1c99deff675cce39954304c3fd71bdfb46749
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-03-15 13:59:56 +0530

    Create K largest elements.cpp

---
+++ Heap/K largest elements.cpp
@@ -0,0 +1,30 @@
+/* Expected Time Complexity: O(N + KlogK)
+Expected Auxiliary Space: O(K+(N-K)*logK) */
+
+
+
+
+class Solution{
+public:	
+	vector<int> kLargest(int arr[], int n, int k) {
+	    priority_queue<int,vector<int>,greater<int>> minheap;
+	    for(int i=0;i<k;i++)
+	        minheap.push(arr[i]);
+	    vector<int> ans;
+	    for(int i=k;i<n;i++){
+	        if(minheap.top()>arr[i])
+	            continue;
+	        else{
+	            minheap.pop();
+	            minheap.push(arr[i]);
+	        }
+	    }
+	    while(minheap.empty()==false){
+	        ans.push_back(minheap.top());
+	        minheap.pop();
+	    }
+	    reverse(ans.begin(),ans.end());
+	    return ans;
+	}
+
+};
