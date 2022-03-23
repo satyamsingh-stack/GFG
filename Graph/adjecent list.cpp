fbb72d71703321ccc56f96ca71edc71bf14dafc1
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-03-23 20:44:46 +0530

    Create adjecent list.cpp

---
+++ Graph/adjecent list.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+void addedge(vector<int> *adj,int u,int v){
+	adj[u].push_back(v);
+	adj[v].push_back(u);
+}
+void print(vector<int> *adj,int v){
+	for(int i=0;i<v;i++){
+		for(int x: adj[i])
+			cout<<x<<" ";
+		cout<<endl;
+	}
+}
+int main(){
+	int v=4;
+	vector<int> adj[v];
+	addedge(adj,0,1);
+	addedge(adj,0,2);
+	addedge(adj,1,2);
+	addedge(adj,1,3);
+	print(adj,v);
+	return 0;
+}
