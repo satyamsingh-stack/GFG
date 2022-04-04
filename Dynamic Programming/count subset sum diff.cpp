7743d10731167d3622e068668c0eb3bd41aab524
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-04-05 00:45:30 +0530

    Update count subset sum diff.cpp

--- Dynamic Programming/count subset sum diff.cpp
+++ Dynamic Programming/count subset sum diff.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int count_subset_sum(int arr[],int n,int sum){
 	int t[n+1][sum+1];
 	for(int i=0;i<=n;i++)
-		t[i][0]=true;
+		t[i][0]=1;
 	for(int i=1;i<=sum;i++)
-		t[0][i]=false;
+		t[0][i]=0;
 	for(int i=1;i<=n;i++){
 		for(int j=1;j<=sum;j++){
 			if(arr[i-1]<=j)
