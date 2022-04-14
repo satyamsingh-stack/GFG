ec6dca810b85ff64e65f61bae945ae646c6f6c86
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-04-14 18:36:37 +0530

    Create Sum of n Natural number.cpp

---
+++ Recursion/Sum of n Natural number.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sum_of_n(int n){
+	if(n==1)
+		return 1;
+	else
+		return n+sum_of_n(n-1);
+}
+int main(){
+	int n;
+	cin>>n;
+	cout<<sum_of_n(n);
+	return 0;
+}
