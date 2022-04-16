cfbf55a38f84a6aedeb2731f79bd2ca86b9fff89
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-04-16 20:11:39 +0530

    Update Coin ChangeII.cpp

--- Dynamic Programming/Coin ChangeII.cpp
+++ Dynamic Programming/Coin ChangeII.cpp
@@ -1,31 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
-int coin_change(int coins[],int amount,int n){
-	int t[n+1][amount+1];
-	for(int i=0;i<=n;i++)
-		t[i][0]=1;
-	for(int i=1;i<=amount;i++)
+int coin_changeII(int coins[],int n,int sum){
+	int t[n+1][sum+1];
+	for(int i=1;i<=n;i++)
+		t[i][0]=0;
+	for(int i=0;i<=sum;i++)
 		t[0][i]=INT_MAX-1;
-	for(int j=1;j<=amount;j++){
+	for(int j=1;j<=sum;j++){
 		if(j%coins[0]==0)
 			t[1][j]=j/coins[0];
 		else
 			t[1][j]=INT_MAX-1;
 	}
 	for(int i=2;i<=n;i++){
-		for(int j=1;j<=amount;j++){
+		for(int j=0;j<=sum;j++){
 			if(coins[i-1]<=j)
 				t[i][j]=min(t[i][j-coins[i-1]]+1,t[i-1][j]);
 			else
 				t[i][j]=t[i-1][j];
 		}
 	}
-	return t[n][amount];
+	return t[n][sum];
 }
 int main(){
-	int coins[]={1,2,5};
-	int amount=11;
+	int coins[]={1,2,3};
+	int sum=5;
 	int n=sizeof(coins)/sizeof(coins[0]);
-	cout<<coin_change(coins,amount,n);
-  return 0;
+	cout<<coin_changeII(coins,n,sum);
+	return 0;
 }
