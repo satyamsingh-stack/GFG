44d795cab6fb609ac191c4657829638083897044
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-07-24 11:13:02 +0530

    Update intersection_of_two_array.cpp

--- Hashmap/intersection_of_two_array.cpp
+++ Hashmap/intersection_of_two_array.cpp
@@ -1,21 +1,19 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-	unordered_set<int> s_a;
-	unordered_set<int> s_b;
-	int arr1[]={10,20,10,30,20};
-	int arr2[]={20,10,10,40};
-	for(int i=0;i<5;i++)
-		s_a.insert(arr1[i]);
-	for(int i=0;i<4;i++)
-		s_b.insert(arr2[i]);
-	int count=0;
-	for(auto x: s_a){
-		for(auto y:s_b){
-			if(x==y)
-				count++;
-		}
-	}
-	cout<<count;
-	return 0;
-}
+class Solution {
+  public:
+    // Function to return the count of the number of elements in
+    // the intersection of two arrays.
+    int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
+        // Your code goes here
+        set<int> st;
+        int count=0;
+        for(int i=0;i<n;i++)
+            st.insert(a[i]);
+        for(int i=0;i<m;i++){
+            if(st.find(b[i])!=st.end()){
+                count++;
+                st.erase(b[i]);
+            }
+        }
+        return count;
+    }
+};
